856dfef66b05027829b464fbfd31c2a3dea2db38
Allan <luminussora@gmail.com>
2020-12-15 00:34:34 -0300

    algoritmo para gerar o grafico

---
+++ pseudo-graph.py
@@ -0,0 +1,56 @@
+import random
+import math
+from matplotlib import pyplot as plt
+
+def rand(n):
+    pseudo = []
+    for i in range(1, n+1):
+        x = random.random()
+        pseudo.append(x)
+    return pseudo
+
+
+def mcl(a, c, m, x0, n):
+    pseudo = []
+    if n < 1:
+        return pseudo
+    x1 = (a*x0 + c) % m
+    x1 = x1 / m
+    pseudo.append(x1)
+    for i in range(2, n+1):
+        x = (a*pseudo[i-2] + c) % m
+        x = x / m
+        pseudo.append(x)
+    return pseudo
+
+
+def handle(rand, cl):
+
+    rand = map(trunc, rand)
+    cl = map(trunc, cl)
+
+    print('\nMétodo random\n')
+    print(list(rand))
+    print('\nMétodo congruente linear\n')
+    print(list(cl))
+    plt.bar([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9], rand)
+    plt.legend()
+    plt.xlabel('Intervalos')
+    plt.ylabel('Frequência')
+    plt.title('Randeom')
+    plt.show()
+
+    plt.bar([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9], cl)
+    plt.legend()
+    plt.xlabel('Intervalos')
+    plt.ylabel('Frequência')
+    plt.title('Congruente Linear')
+    plt.show()
+
+
+def trunc(n):
+    return math.trunc(n*10) / 10
+
+
+handle(rand(100), mcl(2138928109318, 590, 289018098098, 100, 100))
+handle(rand(1000), mcl(2138928109318, 590, 289018098098, 100, 1000))
